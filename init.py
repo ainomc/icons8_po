46628e60c811283c7e9335a3e0792c1dd7b32d32
Kusch-Artem <kuschartem2094@gmail.com>
2017-03-25 18:17:16 +0200

    small fix

--- init.py
+++ init.py
@@ -28,7 +28,7 @@ for file_num in range(len(list_test_fies)):
     list_test_fies[file_num] = os.path.join(os.getcwd(), 'tests', list_test_fies[file_num])
 
 str_list = " ".join(str(x) for x in list_test_fies) # convert list to string
-str_list = os.path.join(os.getcwd(), 'tests', 'tests_sleeklogos_page.py')
+#str_list = os.path.join(os.getcwd(), 'tests', 'tests_sleeklogos_page.py')
 if "win" in platform:
     os.system(r'python -m pytest -v %s -s --showlocals' % str_list)
 elif "linux" in platform:
